351b8878000c77268082e6ef188aae0f4c966027
sivak1106 <92651216+sivarkpocrk@users.noreply.github.com>
2025-04-21 22:39:43 +0100

    adding some csrf

--- quizapp/quizapp/settings.py
+++ quizapp/quizapp/settings.py
@@ -32,6 +32,18 @@ DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 # ALLOWED_HOSTS = ["127.0.0.1"]
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "neotechwave.net",
+    "www.neotechwave.net",
+    "127.0.0.1",
+    "localhost",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://neotechwave.net",
+    "https://www.neotechwave.net",
+]
+
 ALLOWED_HOSTS.extend(
     filter(
         None,
